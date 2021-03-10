a63e147e777921e0e6b2794ad64fc58359d9984b
hodeiez <hodeinaiz@hotmail.com>
2021-03-10 14:45:05 +0100

    more tests on styling

--- test.js
+++ test.js
@@ -24,17 +24,25 @@ function setAllProducts(){
                 for (let i = 0; i < json.length; i++) {
                   categories.push(json[i]);
                   let element=document.createElement('div');
+
                   element.innerHTML ='<div class="card" style="width: 18rem;"><img src="'
-                  +json[i].image+'" class="card-img-top img-fluid" alt="..."><div class="card-body"><h5 class="card-title">'
+                  +json[i].image+'" class="card-img-top" alt="..." height="250rem"><div class="card-body"><h5 class="card-title">'
                   +json[i].title+'</h5><p class="card-text">'
-                  +json[i].description+'</p><a href="#" class="btn btn-dark">buy</a></div></div>';
-                //  element.innerText=json[i].title;
+                  +json[i].description+'</p><div class="row"><a onClick="addToCart('
+                  +json[i].id+');" href="#" class="btn btn-dark col">buy</a><div class="col"><h4>'
+                  +json[i].price+'$</h4></div></div></div></div></div>';
+           
                   element.className=" card-body";
                  document.getElementById("products").appendChild(element);
                   
                 }
               });
+
+              //get all products
               fetch('https://fakestoreapi.com/products')
             .then(res=>res.json())
             .then(json=>console.log(json));
+}
+function addToCart(string){
+    alert(string);
 }

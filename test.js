ea2742c4064e2cb37d63a5ddf0b02a83d332f117
hodeiez <hodeinaiz@hotmail.com>
2021-03-10 21:58:34 +0100

    creted class

--- test.js
+++ test.js
@@ -1,10 +1,40 @@
+class Product {
+  constructor(id, title, description, image, price) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.image = image;
+    this.price = price;
+  }
+  createCard (){
+    return cardTemplate(this.id,this.title,this.description,this.image,this.price);
+  }
+}
+function cardTemplate(id,title,description,image,price){
+  
+return '<div class="card" style="width: 18rem;">'+
+  '<img src="'+image+'" class="card-img-top" alt="'+description+'" height="250rem">'+
+    '<div class="card-body">'+
+      '<h5 class="card-title">'+title+'</h5>'+
+      '<p class="card-text">'+description+'</p>'+
+      '<div class="row">'+
+        '<a onClick="addToCart('+id+');" href="#" class="btn btn-dark col">buy</a>'+
+        '<div class="col">'+
+          '<h4>'+price+'$</h4>'+
+        '</div>'+
+      '</div>'+
+    '</div>'+
+  '</div>'+
+'</div>' ;}
+
+
 function setCategories(){
-    let categories = [];
+    //let categories = [];
     fetch("https://fakestoreapi.com/products/categories")
       .then((res) => res.json())
       .then((json) => {
         for (let i = 0; i < json.length; i++) {
-          categories.push(json[i]);
+       //   categories.push(json[i]);
           let element=document.createElement('li');
           element.className="nav-item";
           element.innerText=json[i];
@@ -17,20 +47,19 @@ function setCategories(){
 }
 
 function setAllProducts(){
-    let categories = [];
+   // let categories = [];
     fetch('https://fakestoreapi.com/products')
             .then(res=>res.json())
             .then(json=>{
                 for (let i = 0; i < json.length; i++) {
-                  categories.push(json[i]);
+                //  categories.push(json[i]);
                   let element=document.createElement('div');
+                  let productItem=new Product(json[i].id, json[i].title, json[i].description, json[i].image, json[i].price)
+                  
+                  //element.innerHTML=cardTemplate(productItem.id,productItem.title,productItem.description,productItem.image,productItem.price);
 
-                  element.innerHTML ='<div class="card" style="width: 18rem;"><img src="'
-                  +json[i].image+'" class="card-img-top" alt="..." height="250rem"><div class="card-body"><h5 class="card-title">'
-                  +json[i].title+'</h5><p class="card-text">'
-                  +json[i].description+'</p><div class="row"><a onClick="addToCart('
-                  +json[i].id+');" href="#" class="btn btn-dark col">buy</a><div class="col"><h4>'
-                  +json[i].price+'$</h4></div></div></div></div></div>';
+                  element.innerHTML=productItem.createCard();
+                 
            
                   element.className=" card-body";
                  document.getElementById("products").appendChild(element);

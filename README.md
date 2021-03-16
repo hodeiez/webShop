7c7961711f35df88d82d5c9d50d05a06eb81ed49
hodeiez <hodeinaiz@hotmail.com>
2021-03-16 13:50:44 +0100

    started with order table and form

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# webShop
-a web shop, to learn and practice Javascript,HTML and CSS.
-task from school, KLIENT UTV
-made to learn javascript,the code structure is not very readable.
-TODO: rewrite following a pattern like MVC, or something to give a cleaner structure to the code
-used https://fakestoreapi.com/ as database
-## check it online
+# webShop </br>
+a web shop, to learn and practice Javascript,HTML and CSS.</br>
+task from school, KLIENT UTV</br>
+made to learn javascript,the code structure is not very readable. </br>
+TODO: rewrite following a pattern like MVC, or something to give a cleaner structure to the code</br>
+used https://fakestoreapi.com/ as database</br>
+## check it online </br>
 https://vigilant-ramanujan-ea3b5a.netlify.app/
--- test.js
+++ test.js
@@ -1,8 +1,12 @@
+//few start variables
 const currency = "€";
 var shopingCartObj = null;
-var productsList=null;
+var productsList = null;
 var cartItemAmount = -1;
-//product class
+const deliveryCosts=15;
+const taxesBase=12;
+
+//created two custom classes, not necessary but it makes the code more readable for me
 class Product {
   constructor(id, title, description, image, price, category) {
     this.id = id;
@@ -28,12 +32,14 @@ class ProductItem {
     this.product = product;
     this.quantity = quantity;
   }
-
 }
 
-function createProductItem(index,quantity){
-    let productItem=new ProductItem(JSON.parse(getLocalData('PRODUCTS_LIST'))[index],quantity)
-    return productItem;
+function createProductItem(index, quantity) {
+  let productItem = new ProductItem(
+    JSON.parse(getLocalData("PRODUCTS_LIST"))[index],
+    quantity
+  );
+  return productItem;
 }
 //VISUAL
 //create HTML for product cards
@@ -47,14 +53,15 @@ function cardTemplate(id, title, description, image, price) {
   let buyButton = clone.querySelector("#buy-node");
   let modalButton = clone.querySelector("#modal-button");
 
-
   titleNode.innerText = title;
   descriptionNode.innerText = description.substr(0, 150) + "...";
   imageNode.src = image;
   priceNode.innerText = price + currency;
   buyButton.value = id;
+  buyButton.dataset.title=title;
   modalButton.dataset.title = title;
   modalButton.dataset.description = description;
+  modalButton.dataset.image=image;
 
   return clone;
 }
@@ -64,39 +71,36 @@ function itemTemplate(product, quantity) {
   let mytemplate = document.querySelector("#product-item");
   let clone = mytemplate.content.cloneNode(true);
   let titleNode = clone.querySelector("#product-item-title");
-  let idNode= clone.querySelector('#product-id');
+  let idNode = clone.querySelector("#product-id");
   let imageNode = clone.querySelector("#product-item-image");
   let quantityNode = clone.querySelector("#product-item-quantity");
- let minusNode=clone.querySelector("#quantity-minus");
- let plusNode=clone.querySelector("#quantity-plus");
- let removeNode = clone.querySelector("#product-item-remove");
+  let minusNode = clone.querySelector("#quantity-minus");
+  let plusNode = clone.querySelector("#quantity-plus");
+  let removeNode = clone.querySelector("#product-item-remove");
 
   titleNode.innerText = product.title;
   imageNode.src = product.image;
-  quantityNode.innerText=quantity;
-  quantityNode.id = "quantity-of-"+product.id;
-  minusNode.id = "minus-"+product.id;
-  plusNode.id = "plus-"+product.id;
-  idNode.id=product.id;
+  quantityNode.innerText = quantity;
+  quantityNode.id = "quantity-of-" + product.id;
+  minusNode.id = "minus-" + product.id;
+  plusNode.id = "plus-" + product.id;
+  idNode.id = product.id;
 
-  plusNode.dataset.productId=product.id;
-  minusNode.dataset.productId=product.id;
-  removeNode.dataset.productId=product.id;
+  plusNode.dataset.productId = product.id;
+  minusNode.dataset.productId = product.id;
+  removeNode.dataset.productId = product.id;
 
-  minusNode.setAttribute('onclick','substractQuantity(this);')
-  plusNode.setAttribute('onclick','sumQuantity(this);')
-  removeNode.setAttribute('onclick','removeItem(this);')
-
+  minusNode.setAttribute("onclick", "substractQuantity(this);");
+  plusNode.setAttribute("onclick", "sumQuantity(this);");
+  removeNode.setAttribute("onclick", "removeItem(this);");
 
   return clone;
 }
 
-
 //update shoppingCartHtml
-function updateShoppingCartItemHTML(productId,newQuantity){
-  $( "#quantity-of-"+productId ).text(newQuantity);
+function updateShoppingCartItemHTML(productId, newQuantity) {
+  $("#quantity-of-" + productId).text(newQuantity);
   updateTotalPrice();
-
 }
 
 //set categories names
@@ -105,57 +109,44 @@ function setCategories() {
   fetch("https://fakestoreapi.com/products/categories")
     .then((res) => res.json())
     .then((json) => {
-      for (let i = 0; i < json.length; i++) {
-        //   categories.push(json[i]);
+      json.forEach((item) => {
         let element = document.createElement("li");
         element.className = "nav-item";
-        element.innerText = json[i];
+        element.innerText = item;
         element.className = "list-group-item";
         document.getElementById("categories").appendChild(element);
-      }
+      });
     });
 }
 
 //On click add item to cart
 function addToCart(e) {
-  //test to show it gets the id
+
   let iconText = document.getElementById("shopping-cart-icon").innerText;
-  
-  alert("Added to shopping cart " + e.value);
-//end test
 
-       let productItem=createProductItem(e.value-1,1);
+  let productItem = createProductItem(e.value - 1, 1);
 
-      //this updates storage
-      if(getItemIndex(productItem.product.id)>=0){
-      updateItemQuantityByIndex(getItemIndex(productItem.product.id),1);
-      
+  //this updates storage
+  if (getItemIndex(productItem.product.id) >= 0) {
+    updateItemQuantityByIndex(getItemIndex(productItem.product.id), 1);
+
     //update the html
-    }
-      else{
-      addItemToShoppingCart(productItem);
+  } else {
+    addItemToShoppingCart(productItem);
 
-//this will go to a function update html shopping cart
-      document
+    //this will go to a function update html shopping cart
+    document
       .getElementById("shopping-cart")
-      .appendChild(itemTemplate(productItem.product,productItem.quantity));
+      .appendChild(itemTemplate(productItem.product, productItem.quantity));
 
-      document.getElementById("shopping-cart-icon").innerText = Number(iconText) + 1;
-      //productItemFunctionality(); 
-    }
-  
+    document.getElementById("shopping-cart-icon").innerText =
+      Number(iconText) + 1;
+ 
+  }
 }
 //change quantity from item
 
-//Open the modal with info
-$("#myModal").on("show.bs.modal", function (event) {
-  var button = $(event.relatedTarget);
-  var title = button.data("title");
-  var description = button.data("description");
-  var modal = $(this);
-  modal.find(".modal-title").text(title);
-  modal.find(".modal-body").text(description);
-});
+
 
 //working with storage
 //setting storage local data getter and setter
@@ -167,52 +158,60 @@ function setLocalData(name, value) {
     ? localStorage.setItem(name, value)
     : localStorage.removeItem(name);
 }
+function clearShoppingCartData(){
+    localStorage.removeItem("SHOPPING_CART_INDEX_COUNT");
+    localStorage.removeItem("SHOPPING_CART");
+}
 //set products: save to local storage, and print in HTML
 function setAllProducts() {
-  productsList=new Array();
-  
-    // let categories = [];
-    fetch("https://fakestoreapi.com/products")
-      .then((res) => res.json())
-      .then((json) => {
-        for (let i = 0; i < json.length; i++) {
-          let productItem = new Product(
-            json[i].id,
-            json[i].title,
-            json[i].description,
-            json[i].image,
-            json[i].price,
-            json[i].category
-          );
-  
-          productsList.push(productItem);
-          let element = productItem.createCard();
-  
-  
-          document.getElementById("products").appendChild(element);
-        }
-        setLocalData("PRODUCTS_LIST", JSON.stringify(productsList));
-      });
-  
-  }
+  productsList = new Array();
 
+  // let categories = [];
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((json) => {
+      json.forEach((product) => {
+        let productItem = new Product(
+          product.id,
+          product.title,
+          product.description,
+          product.image,
+          product.price,
+          product.category
+        );
+
+        productsList.push(productItem);
+        let element = productItem.createCard();
+        document.getElementById("products").appendChild(element);
+      });
+
+      setLocalData("PRODUCTS_LIST", JSON.stringify(productsList));
+    });
+}
 
 //check storage for shopping cart object
 function shoppingCartInit() {
+  if(getLocalData("SHOPPING_CART_INDEX_COUNT")<0){
+    clearShoppingCartData()
+  }
   shopingCartObj = new Array();
   cartItemAmount = 0;
   if (getLocalData("SHOPPING_CART") != null) {
     cartItemAmount = getLocalData("SHOPPING_CART_INDEX_COUNT");
     shopingCartObj = JSON.parse(getLocalData("SHOPPING_CART"));
 
-//update html
-document.getElementById("shopping-cart-icon").innerText = cartItemAmount;
-for(productIndex in shopingCartObj){
+    //update html
+    document.getElementById("shopping-cart-icon").innerText = cartItemAmount<0?0:cartItemAmount;
+    for (productIndex in shopingCartObj) {
       document
-      .getElementById("shopping-cart")
-      .appendChild(itemTemplate(shopingCartObj[productIndex].product,shopingCartObj[productIndex].quantity));
-}
-
+        .getElementById("shopping-cart")
+        .appendChild(
+          itemTemplate(
+            shopingCartObj[productIndex].product,
+            shopingCartObj[productIndex].quantity
+          )
+        );
+    }
   } else {
     console.log("shopping cart not found in storage");
   }
@@ -221,80 +220,141 @@ for(productIndex in shopingCartObj){
 
 //add item to cart storage
 function addItemToShoppingCart(productItem) {
-
-    shopingCartObj[cartItemAmount++] = productItem;
-    setLocalData("SHOPPING_CART", JSON.stringify(shopingCartObj));
-    setLocalData("SHOPPING_CART_INDEX_COUNT", cartItemAmount);
-
+  console.log(cartItemAmount);
+  shopingCartObj[cartItemAmount++] = productItem;
+  setLocalData("SHOPPING_CART", JSON.stringify(shopingCartObj));
+  setLocalData("SHOPPING_CART_INDEX_COUNT", cartItemAmount);
+  updateTotalPrice();
 }
+
 //check if item exists
 function getItemIndex(productId) {
   for (index in shopingCartObj) {
-    if (shopingCartObj[index].product.id == productId)
-      return index;
+    if (shopingCartObj[index].product.id == productId) return index;
   }
   return -1;
 }
-function updateItemQuantityByIndex(index,amount) {
-  console.log('index to: ' +index + ' amount '+ amount);
+
+
+function updateItemQuantityByIndex(index, amount) {
+  console.log("index to: " + index + " amount " + amount);
   shopingCartObj[index].quantity = shopingCartObj[index].quantity + amount;
   setLocalData("SHOPPING_CART", JSON.stringify(shopingCartObj));
   setLocalData("SHOPPING_CART_INDEX_COUNT", cartItemAmount);
 
   //html update
-  updateShoppingCartItemHTML(shopingCartObj[index].product.id,shopingCartObj[index].quantity);
- 
+  updateShoppingCartItemHTML(
+    shopingCartObj[index].product.id,
+    shopingCartObj[index].quantity
+  );
 }
-function removeItemByIndex(index){
-  console.log('index: '+index)
- 
-  let productItem=shopingCartObj[index];
+function removeItemByIndex(index) {
+  console.log("index: " + index);
+
+  let productItem = shopingCartObj[index];
   cartItemAmount--;
 
-  shopingCartObj.splice(index,1);
+  shopingCartObj.splice(index, 1);
 
   setLocalData("SHOPPING_CART", JSON.stringify(shopingCartObj));
-  setLocalData("SHOPPING_CART_INDEX_COUNT", cartItemAmount-1);
-
+  setLocalData("SHOPPING_CART_INDEX_COUNT", cartItemAmount - 1);
 
   //UPDATE SHOPPING CART HTML
- 
-  $('#'+productItem.product.id).remove();
+
+  $("#" + productItem.product.id).remove();
   document.getElementById("shopping-cart-icon").innerText = cartItemAmount;
 
-updateTotalPrice();
-
+  updateTotalPrice();
 }
 
-function updateTotalPrice(){
-  let totalPrice=0;
-  for(index in shopingCartObj){
-totalPrice+=Number(shopingCartObj[index].quantity)*Number(shopingCartObj[index].product.price)
-
+function updateTotalPrice() {
+  let totalPrice = 0;
+  for (index in shopingCartObj) {
+    totalPrice +=
+      Number(shopingCartObj[index].quantity) *
+      Number(shopingCartObj[index].product.price);
   }
-  console.log(totalPrice)
-  $('#shopping-cart-total-price').text(totalPrice.toFixed(2)+currency);
+  console.log(totalPrice);
+  $("#shopping-cart-total-price").text(totalPrice.toFixed(2) + currency);
 }
 
-
-function substractQuantity(e){
-  const productId=e.dataset.productId;
-  const indexOfItem=getItemIndex(productId);
-  if(shopingCartObj[indexOfItem].quantity>1){
-    updateItemQuantityByIndex(indexOfItem,-1);
-  }
-}
-function sumQuantity(e){
-  const productId=e.dataset.productId;
-  const indexOfItem=getItemIndex(productId);
-  if(shopingCartObj[indexOfItem].quantity<50){
-    updateItemQuantityByIndex(indexOfItem,1);
+function substractQuantity(e) {
+  const productId = e.dataset.productId;
+  const indexOfItem = getItemIndex(productId);
+  if (shopingCartObj[indexOfItem].quantity > 1) {
+    updateItemQuantityByIndex(indexOfItem, -1);
   }
 }
-function removeItem(e){
-  const productId=e.dataset.productId;
-  const indexOfItem=getItemIndex(productId);
+function sumQuantity(e) {
+  const productId = e.dataset.productId;
+  const indexOfItem = getItemIndex(productId);
+  if (shopingCartObj[indexOfItem].quantity < 50) {
+    updateItemQuantityByIndex(indexOfItem, 1);
+  }
+}
+function removeItem(e) {
+  const productId = e.dataset.productId;
+  const indexOfItem = getItemIndex(productId);
   removeItemByIndex(indexOfItem);
 }
+//Open the modal with info
+$("#info-modal").on("show.bs.modal", function (event) {
+  var button = $(event.relatedTarget);
+  var title = button.data("title");
+  var description = button.data("description");
+  var image=button.data("image");
+  var modal = $(this);
+  modal.find(".modal-title").text(title);
+  modal.find(".modal-body").text(description);
+  modal.find(".modal-image").attr("src",image);
+});
+$("#added-modal").on("show.bs.modal", function (event) {
+  console.log('fired')
+  var button = $(event.relatedTarget);
+  var title = 'Added to cart';
+  var description = button.data("title")+ ' has been added to your Shopping Cart!';
+  var modal = $(this);
+  modal.find(".modal-title").text(title);
+  modal.find(".modal-body").text(description);
+});
+$('#check-out-button').click(function(){
+  if(shopingCartObj.length>0)
+     window.location.href='order.html'
+    //will add some message  when cart is empty
+     
+  
+})
+function orderInit(){
+  shopingCartObj = JSON.parse(getLocalData("SHOPPING_CART"));
+  fillOrderTable()
+
+}
+
+function fillOrderTable(){
+  let basePrice=0;
+ shopingCartObj.forEach(element=>{
+    basePrice+=(element.product.price * element.quantity)
+  })
+  let taxesPrice=((basePrice/100)*taxesBase).toFixed(2)
+  fillOrderRow()
+  $('#taxes-number').text(taxesPrice+currency)
+  $('#costs-number').text(deliveryCosts+currency)
+  $('#total-price').text(Number(basePrice)+Number(taxesPrice)+Number(deliveryCosts) + currency)
 
 
+
+}
+function fillOrderRow(){
+ 
+  shopingCartObj.forEach(item=>{
+    
+let template=$('#product-row').contents().clone();
+template.find('#product-title-table').text(item.product.title)
+template.find('#product-quantity-table').text(item.quantity)
+template.find('#product-price-table').text(item.product.price)
+template.find('#product-sum-table').text(Number(item.product.price)*item.quantity)
+console.log(template)
+$('#order-table-body').prepend(template)
+})
+
+}

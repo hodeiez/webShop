d16e148dc53cc96a4a1c2b7a2504d3c3a7f0f242
hodeiez <hodeinaiz@hotmail.com>
2021-03-19 11:06:25 +0100

    checkin json link

--- test.js
+++ test.js
@@ -152,7 +152,7 @@ function setAllProducts() {
  //'http://webacademy.se/fakestore/'
   //"backup.json" 
 
-  fetch('https://fakestoreapi.com/products')
+  fetch('http://webacademy.se/fakestore/')
     .then((res) => res.json())
     .then((json) => {
       json.forEach((product) => {
